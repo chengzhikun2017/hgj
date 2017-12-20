56930422d2d015c535932d81b3a08d8e3c1f615c
Lu Chaohai <luchaohai@cn.ibm.com>
2017-12-20 19:10:32 +0800

    添加信用卡2完成

--- src/pages/addCard2.vue
+++ src/pages/addCard2.vue
@@ -1,34 +1,21 @@
 <template>
-  <div class="addCard">
+  <div class="addCard2" flex="dir:top">
     <app-nav title="添加信用卡" flex-box="0"></app-nav>
     <article flex-box="1">
       <div class="form">
-        <div class="input">
-          <label for="">信用卡有效期</label>
-          <input type="text" placeholder="">
+        <app-formitem label="信用卡有效期" :first="true"></app-formitem>
+        <app-formitem label="卡背后三位数字" ></app-formitem>
+        <app-formitem label="账单日" ></app-formitem>
+        <app-formitem label="还款日" ></app-formitem>
+        <app-formitem label="手机号" ></app-formitem>
+        <app-formitem label="验证码" :last="true"></app-formitem>
+        <div class="note" flex>
+          <span class="icon-alert"></span>
+          <p>请务必输入正确的账单日和还款日，否则执行的还款计划会导致自动还款任务未完成的情况下已到还款日</p>
         </div>
-        <div class="input">
-          <label for="">卡背后三位数字</label>
-          <input type="text" placeholder="请输入">
+        <div class="mybutton">
+          <app-button>确认添加</app-button>
         </div>
-        <div class="input">
-          <label for="">账单日</label>
-          <input type="text" placeholder="请选择账单日">
-        </div>
-        <div class="input">
-          <label for="">还款日</label>
-          <input type="text" placeholder="请选择还款日">
-        </div>
-        <div class="input">
-          <label for="">手机号</label>
-          <input type="text" placeholder="请输入银行预留的手机号">
-        </div>
-        <div class="input">
-          <label for="">验证码</label>
-          <input type="text" placeholder="请输入验证码">| <span>重新发送</span>
-        </div>
-        <p>请务必输入正确的账单日和还款日，否则执行的还款计划会导致自动还款任务未完成的情况下已到还款日</p>
-        <button>确认添加</b1utton>
       </div>
     </article>
   </div>
@@ -44,11 +31,26 @@
   }
 </script>
 <style lang="scss" scoped>
-  .addCard {
+  .addCard2 {
     width:100%;
     height: 100%;
     article {
-      
+      overflow: auto;
+      background: #f3f3f3;
+      .note {
+        padding: 0.16rem 0.2rem;
+        font-size: 0.14rem;
+        color: #a4a4a4;
+        .icon-alert {
+          position: relative;
+          top: 0.03rem;
+          padding-right: 0.1rem;
+        }
+      }
+      .mybutton {
+        margin-top: 1.05rem;
+        padding:0 0.2rem;
+      }
     }
   } 
 </style>
