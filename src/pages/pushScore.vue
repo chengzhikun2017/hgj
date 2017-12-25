a57eac55c62f8ab31023f3a7991d8fe32e089120
Lu Chaohai <luchaohai@cn.ibm.com>
2017-12-25 18:56:29 +0800

    剩余页面 未增加样式

---
+++ src/pages/pushScore.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="pushScore" flex="dir:top">
+    <app-nav title="推广积分页面" flex-box="0"></app-nav>
+    <article flex-box="1">
+      <div class="banner"></div>
+      <div class="pushScore-content">
+        <div class="title">
+          <span></span>
+          <h2>积分任务</h2>
+          <button>积分如何使用?</button>
+        </div>
+        <div class="points">
+          <div class="pointItem">
+            <span>分享链接浏览: <span class="data">36次</span></span>
+            <span class="point">+36积分</span>
+          </div>
+          <div class="pointItem">
+            <span>分享被注册: <span class="data">10次</span></span>
+            <span class="point">+10积分</span>
+          </div>
+          <div class="pointItem">
+            <span>微信分享朋友圈: <span class="data">10天</span></span>
+            <span class="point">+10积分</span>
+          </div>
+          <div class="pointItem">
+            <span>升级签到: <span class="data">18天</span></span>
+            <span class="point">+18积分</span>
+          </div>
+        </div>
+      </div>
+    </article>
+  </div>
+</template>
---
+++ src/pages/pushShare.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="pushScore" flex="dir:top">
+    <app-nav title="推广积分页面" flex-box="0"></app-nav>
+    <article flex-box="1">
+      <div class="banner"></div>
+      <div class="pushScore-content">
+        <div class="title">
+          <span></span>
+          <h2>分润记录</h2>
+          <button>提现</button>
+        </div>
+        <div class="points">
+          <p>账号激活分润: <span class="point">+10点</span></p>
+          <p>业务受理分润: <span class="point">+15点</span></p>
+          <p>多几分润合计: <span class="point">+88点</span></p>
+          <p>升级签到: <span class="point">+18点</span></p>
+        </div>
+      </div>
+    </article>
+  </div>
+</template>
