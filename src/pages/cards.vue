cc51608694ea0d60baf6506a1239aa23d1c1a39a
Lu Chaohai <luchaohai@cn.ibm.com>
2018-01-15 18:17:52 +0800

    fix 刚才提交的小问题

--- src/pages/cards.vue
+++ src/pages/cards.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="cards" flex="dir:top">
     <nav flex="cross:center" flex-box="0">
-      <span class="side" flex-box="0" @click='back'>
+      <span class="side" flex-box="0">
+        <!-- <span class="icon" flex="main:center cross:center" @click='back'>
+          <span class="icon-back"></span>
+        </span> -->
       </span>
       <div class="tabs" flex-box="1" flex="main:center">
         <span :class="{'active': choose == 0}" @click="choose=0">我的信用卡{{numCC}}</span>
