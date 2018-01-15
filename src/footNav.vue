d5662c78adb5dd4bec82dfab33715dc4aba62712
Lu Chaohai <luchaohai@cn.ibm.com>
2018-01-15 11:43:51 +0800

    nav layout update

--- src/footNav.vue
+++ src/footNav.vue
@@ -1,22 +1,24 @@
 <template>
-  <div id="footNavVue" v-if='showFootNav'class="container">
-      <span class="cell left-cell" :class="{'active':activeIndex===0}" @click='clickNav(0)'>
+  <div id="footNavVue" v-if='showFootNav' class="container" flex>
+      <span class="cell left-cell" :class="{'active':activeIndex===0}" @click='clickNav(0)' flex-box="0">
         <div class="_icon-box icon left-icon">
           <i class="icon-wallet icon"></i>
           <p class="text">卡包</p>
         </div>
       </span>
-
+      <span flex-box="1" flex="cross:center main:center">
+        <div class="share-bttn" @click='clickNav(1)'>
+          <i class="icon-share share-icon"></i>
+        </div>
+      </span>
       <!-- <span class="cell" :class="{'active':activeIndex===1}" @click='clickNav(1)'>分享</span> -->
-      <span class="cell right-cell" :class="{'active':activeIndex===2}" @click='clickNav(2)'>
+      <span class="cell right-cell" :class="{'active':activeIndex===2}" @click='clickNav(2)' flex-box="0">
         <div class="_icon-box">
           <i class="icon-person icon right-icon"></i>
           <p class="text">我的</p>
         </div>
       </span>
-      <div class="share-bttn" @click='clickNav(1)'>
-        <i class="icon-share share-icon"></i>
-      </div>
+      
   </div>
 </template>
 
@@ -76,7 +78,7 @@ export default {
 <style lang='scss' scoped>
 .container{
   background:#fff;
-  display: flex;
+  /*display: flex;*/
   position: fixed;
   width: 100%;
   height: 0.5rem;
@@ -87,17 +89,19 @@ export default {
   font-size: 0.11rem;
   color:#999;
   z-index: 9999;
+    
   .share-bttn{
+    display: inline-block;
     width: 0.54rem;
     height: 0.41rem;
     border-radius: 6px;
     background-image: linear-gradient(to right, #f86b4b 1%, #f84b4b);
     box-shadow: 0 13px 32px 0 rgba(248, 81, 75, 0.36);
-    position: absolute;
+    /*position: absolute;
     left: 0;right: 0;
     top: 0;bottom: 0;
-    margin:auto;
-    margin-bottom: 0.05rem;
+    margin:auto;*/
+    /*margin-bottom: 0.05rem;*/
     .share-icon{
       line-height: 0.41rem;
       text-align: center;
@@ -107,7 +111,7 @@ export default {
     }
   }
   .cell{
-    width: 50%;
+    /*width: 50%;*/
     display: inline-block;
 
     /*border:1px solid red;*/
